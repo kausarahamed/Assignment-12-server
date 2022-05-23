dfb0d054edd0b247471515ea19438b534304c870
kausarahamed <galibhasan517@gmail.com>
2022-05-23 21:02:53 +0600

    get

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -18,10 +18,17 @@ async function run() {
   try {
     await client.connect();
     const airCollection = client.db("aircool").collection("air");
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await airCollection.findOne(query);
+      res.send(result);
+    });
   } finally {
     //    client.close();
   }
 }
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running ac Server");
