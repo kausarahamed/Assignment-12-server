aef3bc36c8fa93adf5ab3f2149e21e54e7b6c8a6
kausarahamed <galibhasan517@gmail.com>
2022-05-26 01:14:49 +0600

    add product success

--- Index.js
+++ Index.js
@@ -25,7 +25,7 @@ async function run() {
     app.get("/purchase/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await airCollection.findOne(query);
+      const result = await adminProductCollection.findOne(query);
       res.send(result);
     });
     app.get("/product", async (req, res) => {
