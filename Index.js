b48ec3c9a663bfc5ee7329d3d3cc4c035d929021
kausarahamed <galibhasan517@gmail.com>
2022-05-27 17:55:40 +0600

    heroku

--- Index.js
+++ Index.js
@@ -133,7 +133,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Running ac Server");
+  res.send("Running ac Server ......");
 });
 
 app.listen(port, () => {
