f71f265c78b047dc045daaec1955047728ba8812
kausarahamed <galibhasan517@gmail.com>
2022-05-28 18:03:49 +0600

    admin done

--- Index.js
+++ Index.js
@@ -127,23 +127,13 @@ async function run() {
       res.send(users);
     });
 
-    // app.put("/owner/admin/:email", async (req, res) => {
-    //   const email = req.params.email;
-    //   // const requester = req.decoded.email;
-    //   const requesterAccount = await userCollection.findOne({
-    //     email: requester,
-    //   });
-    //   if (requesterAccount.role === "admin") {
-    //     const filter = { email: email };
-    //     const updateDoc = {
-    //       $set: { role: "admin" },
-    //     };
-    //     const result = await userCollection.updateOne(filter, updateDoc);
-    //     res.send(result);
-    //   } else {
-    //     res.status(403).send({ message: "forbidden" });
-    //   }
-    // });
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     app.put("/owner/admin/:email", async (req, res) => {
       const email = req.params.email;
       const requester = req.body.email;
